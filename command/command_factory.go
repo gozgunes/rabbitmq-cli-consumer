5eb3d94fecafdcc0a11aa175fad57b0e9ec931cd
Gokhan Ozgunes <gyokhan.yozgyunesh@elefant.ro>
2018-01-11 10:59:04 +0200

    #36 pass messages to STDIN

--- command/command_factory.go
+++ command/command_factory.go
@@ -10,5 +10,16 @@ type CommandFactory struct {
 }
 
 func (me CommandFactory) Create(body string) *exec.Cmd {
-	return exec.Command(me.Cmd, append(me.Args, body)...)
+	cmd := exec.Command(me.Cmd, me.Args...)
+
+	stdin := cmd.StdinPipe()
+
+	go func() {
+		if(len(body) > 0) {
+			defer stdin.Close()
+			io.WriteString(stdin, body)
+		}
+	}()
+
+	return cmd
 }
